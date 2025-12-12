60a199757f5d0cf8f82eeb532452d05727763dda
Felipe <plangloi@student.42.fr>
2025-12-12 16:37:03 +0100

    Add .nojekyll to disable Jekyll

--- Portfolio/vite.config.js
+++ Portfolio/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: '/Portfolio/',   // ← Doit correspondre au nom du repo
+    base: '/Portfolio/',  
     plugins: [react()],
 
     // Options du serveur de développement
